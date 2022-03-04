3ce15f6efec620e7d38bb64b0e69b2cbacdd69af
Leilabbasi <99692454+Leilabbasi@users.noreply.github.com>
2022-03-04 16:03:32 +0330

    Add files via upload

---
+++ .py6.py
@@ -0,0 +1,17 @@
+val=int(input('please enter a number'))
+if val < 10 :
+    if val !=5 :
+        print ("wow" , end=' ')
+    else :
+        val += 1
+else :
+    if val== 17:
+        val += 10
+    else :
+        print("whoa" , end=' ')
+print (val)
+#if nimber is '3' => wow 3
+#if nimber is '21' => whoa 21
+#if nimber is '5' => 6
+#if nimber is '17' => 27
+#if nimber is '-5' => wow -5
