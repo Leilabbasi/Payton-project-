377dc1321b5efcfd6b5383de5ae04c5fd71a3ea4
Leilabbasi <99692454+Leilabbasi@users.noreply.github.com>
2022-03-05 20:06:22 +0330

    Add files via upload

---
+++ .py7.py
@@ -0,0 +1,9 @@
+sidea = int(input('plaese enter side(a) triangle : '))
+sideb = int(input('plaese enter side(b) triangle : '))
+sidec = int(input('plaese enter side(c) triangle : ')) 
+if sidea == sideb == sidec :
+    print('right triangle')
+else :
+    print('not right')
+    
+    
