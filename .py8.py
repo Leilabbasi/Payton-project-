70d78761738bc402efabeaa984a659932ba15e8e
Leilabbasi <99692454+Leilabbasi@users.noreply.github.com>
2022-03-06 23:04:04 +0330

    Add files via upload

--- .py8.py
+++ .py8.py
@@ -5,16 +5,7 @@ value4=int(input('please enter value4 : '))
 value5=int(input('please enter value5 : '))
 way1= ('DUPLICATE')
 way2= ('ALL UNIQU')
-if value1 == value2 :
-    if value1 == value3 :
-        if value1 == value4 :
-            if value1 == value5 :
-                if value2 == value3 :
-                    if value2 == value4 :
-                        if value2 == value5 :
-                            if value3 == value4 :
-                                if value3 == value5 :
-                                    if value4 == value5 :
-                                        print(way1)
+if value1 == value2 or value1 == value3 or value1 == value4 or value1 == value5 or value2 == value3 or value2 == value4 or value2 == value5 or value3 == value4 or value3 == value5 or value4 == value5 :
+    print(way1)
 else:
     print(way2)
