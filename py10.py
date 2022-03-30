f2315d30d5770d2b4ea888be7f8ed4538ec60e90
Leilabbasi <99692454+Leilabbasi@users.noreply.github.com>
2022-03-30 17:34:17 +0430

    Add files via upload

---
+++ py10.py
@@ -0,0 +1,14 @@
+import math
+x=float(input('please enter a number :  '))
+pi=math.pi
+x1= (pi/180)
+sin=0
+a=1
+for b in range(1,10,3):
+    c=1
+    for t in range(1,b+1):
+        c1=t
+    sin+=a*(x**b)/c
+    a1=-1
+    print('sin',sin)
+    
