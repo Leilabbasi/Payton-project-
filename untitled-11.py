25fc51d7831c5b92aad1b0093ded513ebbbd1477
Leilabbasi <99692454+Leilabbasi@users.noreply.github.com>
2022-05-08 22:07:56 +0430

    Add files via upload

--- untitled-11.py
+++ untitled-11.py
@@ -1,9 +1,23 @@
 #18
+
+    
 def next_number():
-    lst=[1,3,4,5,7]
-    sp=1
-    for i in lst:
-        if i==1 not in lst:
-            pribt(sp)
+        lst=[1,3,5,6]
+        m=0
+        for i in lst:
+                if i> m:
+                        m=i
+            
+            
+        for i in range(1,m+1):
+                if i not in lst:
+                        return i
+        print(i)
     
 print(next_number())
+                
+        
+        
+    
+    
+    
