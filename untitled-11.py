ed98d20bf253c2f0134f8f3b00cfa77ea9e1a203
Leilabbasi <99692454+Leilabbasi@users.noreply.github.com>
2022-05-09 21:58:01 +0430

    Add files via upload

--- untitled-11.py
+++ untitled-11.py
@@ -15,9 +15,22 @@ def next_number():
         print(i)
     
 print(next_number())
+#______________________________________________
+
+
+#22
                 
-        
-        
-    
-    
-    
+def Q22(m):
+        new_mat = [[0] * len(m)] * len(m)
+        for i in range(len(m)):
+                for j in range(len(m[0])):
+                        new_mat[j][i] = m[i][j]
+        flag = 0
+        for i in range(len(m)):
+                for j in range(len(m)):
+                        if m[i] == new_mat[j]:
+                                flag = 1
+        if flag:
+                return True
+        else:
+                return False
