99b70236f6dbe50d3490da5ac68971775fbce06c
Leilabbasi <99692454+Leilabbasi@users.noreply.github.com>
2022-06-06 00:05:36 +0430

    Add files via upload

---
+++ py 16-22.py
@@ -0,0 +1,74 @@
+#16
+d={3:0, 5:1, 10:1, 8:2, 15:4}
+
+#a
+print(d)
+#answer ==> {3: 0, 5: 1, 10: 1, 8: 2, 15: 4}
+
+#b
+for x in d :
+    print(x)
+#answer ==> 3 5 10 8 15
+
+#c
+for x in d.keys():
+    print(x)
+#answer ==> 3 5 10 8 15 
+
+#d
+for x in d.values():
+    print(x)
+#answer ==> 0 1 1 2 4 
+
+#17
+#A dictionary is a collection which is ordered
+
+#22
+#Sets are mutable
+
+#23
+
+#a ==>{20, 19, 2, 10, 7}
+
+#b ==> true
+
+#c ==> false
+
+#d ==>{10, 7}
+
+#e ==>{2, 4, 5, 6, 7, 9, 10, 19, 20}
+
+#f ==> true
+
+#g ==> true
+
+#h ==> false
+
+#i ==> true
+
+#j ==> false
+
+#k ==> 5
+
+#l ==>{2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
+
+#m ==>{0, 5, 8, 17, 18}
+
+#n ==>{0, 5}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
