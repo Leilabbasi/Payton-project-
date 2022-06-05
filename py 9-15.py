073b1b891f3fbfefb7dbd3524b54c8f254a669e7
Leilabbasi <99692454+Leilabbasi@users.noreply.github.com>
2022-06-06 00:04:39 +0430

    Add files via upload

---
+++ py 9-15.py
@@ -0,0 +1,33 @@
+#9
+def zero_sum():
+    s=0
+    for i in tpl :
+        s=s+i
+    if s == 0 :
+        print('true')
+    else : 
+        print('false')
+        
+        
+tpl=(1,2,-3)
+answer= zero_sum()
+print(answer)
+    
+#10
+#Associative container" redirects here. For the implementation of ordered associative arrays in the standard library of the C++ programming language
+
+#11
+#To create a dictionary with items, you need to include key-value pairs inside the curly braces.
+dictionary_d = { key : value}
+
+#12
+print(type(my_dictionary))
+
+#13
+#Python's official documentation says that the KeyError is raised when a mapping key is accessed and isn't found in the mapping
+
+#14
+#Python's official documentation says that the KeyError is raised when a mapping key is accessed and isn't found in the mapping
+
+#15
+#Dictionaries themselves are mutable
